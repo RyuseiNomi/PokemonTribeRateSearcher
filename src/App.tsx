ded49b4c009fef5044db282228a00a3f2c4a9878
RyuseiNomi <kanontonto1010@gmail.com>
2021-06-01 13:40:40 +0900

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@ import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
 import PokemonSelector from './PokemonSelector';
+import { Layout } from 'antd';
 
 function App() {
+
+  const { Footer } = Layout;
+  const footerStyle = {
+    textAlign: 'center',
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+  }
 
   return (
     <div className="App">
         <p>ポケモン種族値早見ツール</p>
         <PokemonSelector />
+        <Footer style={footerStyle}>©2021 Ryusei Nomi</Footer>
     </div>
   );
 }
