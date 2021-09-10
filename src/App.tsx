313691f69bf2e2d60e496a80ab51badd66ae23b7
RyuseiNomi <kanontonto1010@gmail.com>
2021-09-10 13:22:27 +0900

    fix: ci

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import SingleSearch from 'pages/SingleSearch'
 import CompareRate from 'pages/CompareRate'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
 
